5af14736309d48eb4f154104fe6a308d22d5dd1f
sharky <grzegorz.dyk@gmail.com>
2017-03-10 19:04:54 +0100

    Wrapped image cropping into a class

--- click_and_crop.py
+++ click_and_crop.py
@@ -5,81 +5,93 @@
 import argparse
 import cv2
 
-# initialize the list of reference points and boolean indicating
-# whether cropping is being performed or not
-regions = []
-cropping = False
-startPoint = None
-currentCrop = None
+
+class Cropper(object):
+    def __init__(self, window_name):
+        self.__windowName = window_name
+        self.__currentCrop = None
+        self.__cropping = False
+        self.__startPoint = None
+        self.__onCrop = lambda x: x
+        self.__image = None
+
+    def on_crop(self, listener):
+        self.__onCrop = listener
+
+    def change_image(self, image):
+        self.__image = image
+        self.__reset()
+        self.__refresh_image()
+
+    def start(self, image):
+        self.__image = image
+        cv2.namedWindow(self.__windowName)
+        cv2.setMouseCallback(self.__windowName, lambda event, x, y, req, param : self.__click_and_crop(event, x, y))
+        while True:
+            # display the image and wait for a keypress
+            self.__refresh_image()
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the 'r' key is pressed, reset the cropping region
+            if key == ord("r"):
+                self.__currentCrop = None
+                self.__startPoint = None
+
+            # if the 'c' key is pressed, break from the loop
+            elif key == ord("c"):
+                if self.__currentCrop is not None:
+                    self.__onCrop(self.__currentCrop)
+                    self.__reset()
+
+    def __click_and_crop(self, event, x, y):
+        # if the left mouse button was clicked, record the starting
+        # (x, y) coordinates and indicate that cropping is being
+        # performed
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.__startPoint = (x, y)
+            self.__cropping = True
+
+        if self.__cropping:
+            self.__currentCrop = (self.__startPoint, (x, y))
+
+        # check to see if the left mouse button was released
+        if event == cv2.EVENT_LBUTTONUP:
+            # record the ending (x, y) coordinates and indicate that
+            # the cropping operation is finished
+            self.__cropping = False
+            self.__startPoint = None
+
+    def __reset(self):
+        self.__cropping = False
+        self.__currentCrop = None
+        self.__startPoint = None
+
+    def __refresh_image(self):
+        if self.__currentCrop is not None:
+            cloned = self.__image.copy()
+            cv2.rectangle(cloned, self.__currentCrop[0], self.__currentCrop[1], (0, 255, 0), 2)
+            cv2.imshow(self.__windowName, cloned)
+        else:
+            cv2.imshow(self.__windowName, self.__image)
 
 
-def click_and_crop(event, x, y, req, param):
-    # grab references to the global variables
-    global cropping, startPoint, regions, currentCrop
-
-    # if the left mouse button was clicked, record the starting
-    # (x, y) coordinates and indicate that cropping is being
-    # performed
-    if event == cv2.EVENT_LBUTTONDOWN:
-        startPoint = (x, y)
-        currentCrop = (startPoint, startPoint)
-        cropping = True
-
-    if cropping:
-        currentCrop = (startPoint, (x, y))
-
-    # check to see if the left mouse button was released
-    if event == cv2.EVENT_LBUTTONUP:
-        # record the ending (x, y) coordinates and indicate that
-        # the cropping operation is finished
-        regions.append(currentCrop)
-        cropping = False
-        startPoint = None
-
-    refresh_image()
+if __name__ == "__main__":
+    # construct the argument parser and parse the arguments
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True, help="Path to the image")
+    args = vars(ap.parse_args())
 
 
-def refresh_image():
-    global currentCrop, image
-    if currentCrop is not None:
-        cloned = image.copy()
-        cv2.rectangle(cloned, currentCrop[0], currentCrop[1], (0, 255, 0), 2)
-        cv2.imshow("image", cloned)
-    else:
-        cv2.imshow("image", image)
+    def display_crop(crop):
+        roi = image[crop[0][1]:crop[1][1], crop[0][0]:crop[1][0]]
+        cv2.imshow("ROI", roi)
 
+    # load the image, clone it, and setup the mouse callback function
+    image = cv2.imread(args["image"])
+    cropper = Cropper("image")
+    cropper.on_crop(display_crop)
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True, help="Path to the image")
-args = vars(ap.parse_args())
+    cropper.start(image)
 
-# load the image, clone it, and setup the mouse callback function
-image = cv2.imread(args["image"])
-clone = image.copy()
-cv2.namedWindow("image")
-cv2.setMouseCallback("image", click_and_crop)
-
-# keep looping until the 'q' key is pressed
-while True:
-    # display the image and wait for a keypress
-    refresh_image()
-    key = cv2.waitKey(1) & 0xFF
-
-    # if the 'r' key is pressed, reset the cropping region
-    if key == ord("r"):
-        image = clone.copy()
-
-    # if the 'c' key is pressed, break from the loop
-    elif key == ord("c"):
-        break
-
-# if there are two reference points, then crop the region of interest
-# from teh image and display it
-if currentCrop is not None:
-    roi = image[currentCrop[0][1]:currentCrop[1][1], currentCrop[0][0]:currentCrop[1][0]]
-    cv2.imshow("ROI", roi)
-    cv2.waitKey(0)
-
-# close all open windows
-cv2.destroyAllWindows()
+    # close all open windows
+    cv2.destroyAllWindows()
