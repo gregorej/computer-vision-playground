f7c7c75df7a43da7a00c1b8033c9206381265f9d
sharky <grzegorz.dyk@gmail.com>
2017-03-23 21:27:57 +0100

    Added iterator and indexing for CamVid dataset

--- seg_net.py
+++ seg_net.py
@@ -54,49 +54,81 @@ class CamVidDataset(object):
 
     class Generator(object):
 
-        def __init__(self, owner):
+        def __init__(self, owner, batch_size):
             self.__current_index = 0
             self.owner = owner
+            self.batch_size = batch_size
 
         def next(self):
             img_size = self.owner.img_size
             if self.__current_index >= self.owner.size:
                 self.__current_index = 0
-            actual_batch_size = min(self.owner.batch_size, self.owner.size - self.__current_index)
+            actual_batch_size = min(self.batch_size, self.owner.size - self.__current_index)
             image_data = np.zeros((actual_batch_size, img_size[0], img_size[1], 3), dtype='uint8')
             label_data = np.zeros((actual_batch_size, img_size[0], img_size[1]), dtype='uint8')
-            for idx, img_file in enumerate(self.owner._images[self.__current_index:actual_batch_size]):
-                full_path = os.path.join(self.owner._images_dir, img_file)
-                image_data[idx] = cv2.imread(full_path)
-            for idx, img_file in enumerate(self.owner._labels[self.__current_index:actual_batch_size]):
-                full_path = os.path.join(self.owner._labels_dir, img_file)
-                label_data[idx] = cv2.cvtColor(cv2.imread(full_path), cv2.COLOR_BGR2GRAY)
+            for idx in range(0, actual_batch_size):
+                image, labels = self.owner[self.__current_index + idx]
+                label_data[idx] = labels
+                image_data[idx] = image
             self.__current_index += actual_batch_size
             return image_data, label_data
 
         def __iter__(self):
             return self
 
-    def __init__(self, batch_size):
-        self.__current_index = 0
-        self.batch_size = batch_size
-        datasets_home = os.environ['DATASETS'] + '/camvid'
-        self._images_dir = datasets_home + '/701_StillsRaw_full'
-        self._images = [f for f in os.listdir(self._images_dir) if f.endswith('.png')]
+    class Iterator(object):
+
+        def __init__(self, owner):
+            self.__current_index = 0
+            self.owner = owner
+
+        def next(self):
+            if self.__current_index >= self.owner.size:
+                raise StopIteration
+            result = self.owner[self.__current_index]
+            self.__current_index += 1
+            return result
+
+    def __init__(self, image_files, label_files, dataset_home):
+        self._images_dir = dataset_home + '/701_StillsRaw_full'
+        self._images = image_files
         self._images.sort()
-        self._labels_dir = datasets_home + '/LabeledApproved_full'
-        self._labels = [f for f in os.listdir(self._labels_dir) if f.endswith('.png')]
+        self._labels_dir = dataset_home + '/LabeledApproved_full'
+        self._labels = label_files
         self._labels.sort()
+        self._dataset_home = dataset_home
         self.size = len(self._images)
+
+    @classmethod
+    def from_dir(cls):
+        datasets_home = os.environ['DATASETS'] + '/camvid'
+        images_dir = datasets_home + '/701_StillsRaw_full'
+        labels_dir = datasets_home + '/LabeledApproved_full'
+        return CamVidDataset([f for f in os.listdir(images_dir) if f.endswith('.png')],
+                             [f for f in os.listdir(labels_dir) if f.endswith('.png')], datasets_home)
 
     def __len__(self):
         return self.size
 
-    def generator(self):
-        return self.Generator(self)
+    def __iter__(self):
+        return self.Iterator(self)
 
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            return CamVidDataset(self._images[item], self._labels[item], self._dataset_home)
+        if isinstance(item, int) and 0 <= item < self.size:
+            full_image_path = os.path.join(self._images_dir, self._images[item])
+            image = cv2.imread(full_image_path)
+            full_label_path = os.path.join(self._labels_dir, self._labels[item])
+            labels = cv2.cvtColor(cv2.imread(full_label_path), cv2.COLOR_BGR2GRAY)
+            return image, labels
+        else:
+            raise IndexError('invalid index')
 
-camvid = CamVidDataset(10)
+    def generator(self, batch_size=10):
+        return self.Generator(self, batch_size)
+
+camvid = CamVidDataset.from_dir()[0:-100]
 model = seg_net(camvid.img_size)
 left_for_test = 1
 model.compile(loss='mean_squared_error', optimizer='sgd')
@@ -106,3 +138,5 @@ model.fit_generator(camvid.generator(),
 
 model.save('seg_net.mine.h5')
 model.save_weights("seg_net.mine.weights.h5", overwrite=True)
+
+print model.evaluate_generator(camvid[len(camvid) - 100:len(camvid)].generator(10), 10)
