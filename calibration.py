d5d3f22372adca76ab4df4fa0b702344828cb75c
sharky <grzegorz.dyk@gmail.com>
2017-07-26 23:29:23 +0200

    Slight refactoring

--- calibration.py
+++ calibration.py
@@ -8,45 +8,55 @@ chessboardSize = (9,6)
 BLACK = (0,0,0)
 GREEN = (0,255, 0)
 
-def calibrateCamera(objpts , imgpts , size):
-    ret,mtx,dist,_,_ =  cv2.calibrateCamera(objpts , imgpts, size,None,None)
-    if (ret):
-       dict = {}
-       dict['mtx'] = mtx
-       dict['dist'] = dist
-       return dict
+
+def calibrateCamera(objpts, imgpts, size):
+    ret, mtx, dist, _, _ = cv2.calibrateCamera(objpts, imgpts, size,None,None)
+    if ret:
+        return {'mtx': mtx, 'dist': dist}
+
 
 def drawCorners(img, corners):
     for c in corners:
         center = tuple(c[0])
-        print center
+        #print center
         cv2.circle(img, center, 4, GREEN, -1) # -1 means fill
-    cv2.imshow('chessboard',img)
+    cv2.imshow('chessboard', img)
     cv2.waitKey(1)
 
-def calibrate(path ,size, save = True,ignore_old_calib = True):
-    if(ignore_old_calib == False):
+
+def calibrate(path, save=True, ignore_old_calib=True):
+    if not ignore_old_calib:
         lst = glob.glob('./*.p')
-        if(lst):
-            return  pickle.load(open('CalibrationParameters.p','rb'))
+        if lst:
+            return pickle.load(open('CalibrationParameters.p','rb'))
     objpts = []
     imgpts = []
     objp = np.zeros((chessboardSize[1]*chessboardSize[0],3), np.float32)
-    objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1,2)
-    files = glob.glob(path + '/*.jpg')
+    objp[:, :2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1, 2)
+    files = glob.glob(path + '/*.png')
+    size = None
     for imgFile in files:
         img = cv2.imread(imgFile)
-        gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        ret , corners = cv2.findChessboardCorners(gray, chessboardSize,None)
-        if(ret == True):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        print imgFile
+        size = gray.shape
+        print size
+        ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
+        if ret:
             drawCorners(gray, corners)
             objpts.append(objp)
             imgpts.append(corners)
-    dict = calibrateCamera(objpts , imgpts , size)
-    print dict
-    if (save == True):
-        pickle.dump(dict , open('CalibrationParameters.p','wb'))
-    return dict
+    result = calibrateCamera(objpts, imgpts, size)
+    if save:
+        pickle.dump(result, open('CalibrationParameters.p','wb'))
+    return result
 
 if __name__ == "__main__":
-    calibrate('./calibration/dell',(540,960))
+    import argparse
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-p", "--pictures-dir", required=True, help="Directory with pictures used to calibrate")
+    args = vars(ap.parse_args())
+    result = calibrate(args['pictures_dir'], save=False)
+    print result['mtx']
+    print result['dist']
